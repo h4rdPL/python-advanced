ffbb2458d2e15643a1fec759463cdc7bda8d2e85
Mateusz Mamica <mateusz.mamica18@gmail.com>
2025-03-04 11:38:03 +0100

    exercise 2 - channels

---
+++ zadanie2.py
@@ -0,0 +1,11 @@
+import cv2
+
+image = cv2.imread("image.jpg")
+
+if image is None:
+    print("Błąd: nie można wczytać obrazu!")
+else:
+    print("Obraz wczytano poprawnie.")
+
+(h, w, c) = image.shape[:3]
+print(f'channels: {c}')
